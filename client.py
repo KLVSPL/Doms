9702c090259805aa07a0df218f6c38811228e5d0
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-02 07:47:15 +0800

    Update client.py

--- client.py
+++ client.py
@@ -6,16 +6,18 @@ server_port = 10242
 #get ip
 s.connect((server_ip, server_port))
 hostname = s.recv(1024)
+print("Host/IP:",str(hostname.decode()))
 s.close()
 #Get port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server_ip, server_port))
 hostport = s.recv(1024)
+print("Port:",int(hostport.decode()))
 s.close()
 #Get Protocol
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server_ip, server_port))
 proto = s.recv(1024)
+print("Protocol:",str(proto.decode()))
 s.close()
 
-print(str(hostname.decode()),":",int(hostport.decode()),":",str(proto.decode()))
