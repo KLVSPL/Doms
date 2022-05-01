240ed5291fa26f9e51026c514380348285ea9693
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-01 21:21:25 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 import socket
-s = socket.socket()
-port = 10242
-s.connect(('127.0.0.1', port))
-ip = s.recv(1024)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_ip = "127.0.0.1"
+server_port = 10242
+
+#get ip
+s.connect((server_ip, server_port))
+hostname = s.recv(1024)
 s.close()
-s = socket.socket()
-s.connect(('127.0.0.1', port))
-port = s.recv(1024)
+#Get port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_ip, server_port))
+hostport = s.recv(1024)
+s.close()
+#Get Protocol
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_ip, server_port))
+proto = s.recv(1024)
 s.close()
 
-print(ip.decode(),":",port.decode())
+print(str(hostname.decode()),":",int(hostport.decode()),":",str(proto.decode()))
