23a377f566108d07129c38922ea2787f46a53ecc
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-02 08:03:33 +0800

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,24 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_ip = "127.0.0.1"
-server_port = 10242
+server_ip = "0.tcp.ap.ngrok.io"
+server_port = int(input("Enter Server Port:"))
 
 #get ip
 s.connect((server_ip, server_port))
 hostname = s.recv(1024)
-print("Host/IP:",str(hostname.decode()))
+ip = str(hostname.decode())
 s.close()
 #Get port
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server_ip, server_port))
 hostport = s.recv(1024)
-print("Port:",int(hostport.decode()))
+port = int(hostport.decode())
 s.close()
 #Get Protocol
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server_ip, server_port))
 proto = s.recv(1024)
-print("Protocol:",str(proto.decode()))
+protocol = str(proto.decode())
 s.close()
 
+print(ip,port,protocol)
