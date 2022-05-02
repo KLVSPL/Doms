ab7196ffe2ae6d6a8d7d19280024a0a6ad1b2b49
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-05-02 08:04:54 +0800

    Update 443c.py

--- 443c.py
+++ 443c.py
@@ -11,25 +11,23 @@ server_ip = "0.tcp.ap.ngrok.io"
 server_port = int(input("Enter Server Port:"))
 print("Bot Connected")
 
-#get ip
+#Get ip
 s.connect((server_ip, server_port))
 hostname = s.recv(1024)
-s.close()
-#======================================================
-#Get port
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect((server_ip, server_port))
-#hostport = s.recv(1024)
-#s.close()
-#Get Protocol
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect((server_ip, server_port))
-#proto = s.recv(1024)
-#s.close()
-#======================================================
 ip = str(hostname.decode())
-port = 443
-protocol = "GET"
+s.close()
+#Get port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_ip, server_port))
+hostport = s.recv(1024)
+port = int(hostport.decode())
+s.close()
+#Get Protocol
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_ip, server_port))
+proto = s.recv(1024)
+protocol = str(proto.decode())
+s.close()
 
 print(ip,":",port,":",protocol)
 
